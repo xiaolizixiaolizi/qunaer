b5eec122db537aa4d167f056524cc0dbdc487471
xiaoliziaaa <1756329434@qq.com>
2019-10-28 20:23:41 +0800

    homeicon

---
+++ src/views/home/homeComps/HomeIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="HomeIcon">
+    <h3>HomeIcon</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeIcon",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {
+    // *******************************网络请求*******************************
+    // *******************************逻辑处理*******************************
+  },
+  computed: {},
+  watch: {},
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  updated() {}, //生命周期 - 更新之后
+  activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style lang='stylus' scoped>
+
+//@import ; 引入公共css类
+
+
+
+</style>
--- test.js
+++ test.js
@@ -1,5 +1,10 @@
-const qs=require('qs')
-let  url='https://touch.dujia.qunar.com/list?modules=list%2CbookingInfo%2CactivityDetail&dep=%E4%B8%8A%E6%B5%B7&query=%E5%B7%B4%E5%8E%98%E5%B2%9B&type=all&dappDealTrace=true&it=n_index_desti_recom&date=&ddf=true&needNoResult=true&originalquery=%E5%B7%B4%E5%8E%98%E5%B2%9B&limit=100,20&qsact=scroll'
-
-let res=qs.parse(url)
-console.log(res)
+/*
+查看所有分支 ：git branch
+创建分支 git branch ***
+git checkout 分支名字
+git checkout -b 分知名bbb 创建并且切到新的分支
+切到主分支： git checkout master
+git merge origin/bbb 合并分支到master主分支
+git commit =m ""
+git push
+*/
